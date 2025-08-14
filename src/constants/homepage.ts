69c5f3e7bff256cf30cdb34d86e90051e5d07475
Reece Harding <reeceharding@gmail.com>
2025-08-14 15:29:57 -0500

    Update homepage video to new walkthrough demo

--- src/constants/homepage.ts
+++ src/constants/homepage.ts
@@ -1,7 +1,7 @@
 // Homepage video configuration
 export const HOMEPAGE_VIDEO = {
-  // Replace this with your new Mux playback ID
-  playbackId: '01ddBxgG7W53ZCMZ02LLP692sLD4w009XzUtoCd00NcSBO8',
+  // Updated with new walkthrough video
+  playbackId: 'EsbS7MBDCWyn8IPdnYPfKnv2v01500IQOY00YzXrkheM48',
   // Mux automatically generates thumbnails - just replace the playback ID in the URL
   getThumbnailUrl: (playbackId: string, time: number = 10) => 
     `https://image.mux.com/${playbackId}/thumbnail.png?time=${time}`,
