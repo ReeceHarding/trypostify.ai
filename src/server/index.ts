732b69138fe1e1059ece7534f21192d284df5223
Reece Harding <reeceharding@gmail.com>
2025-08-14 15:13:19 -0500

    Add contentport-2.vercel.app to API CORS allowed origins

--- src/server/index.ts
+++ src/server/index.ts
@@ -16,6 +16,8 @@ const api = j
       origin: [
         process.env.NEXT_PUBLIC_SITE_URL,
         process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined,
+        'https://contentport-2.vercel.app',
+        'https://www.contentport-2.vercel.app',
         'https://trypostify.ai',
         'https://www.trypostify.ai',
         'https://trypostify.vercel.app',
